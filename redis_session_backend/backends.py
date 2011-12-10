8ae347ffa38500ee51890e395ff3fd8e3f3988cd
Lispython <alex@obout.ru>
2011-12-10 13:42:14 +0400

    Django 1.4 compatible

--- redis_session_backend/backends.py
+++ redis_session_backend/backends.py
@@ -22,18 +22,20 @@ import redis
 class SessionStore(SessionBase):
     """
     A redis-based session store.
-    """
+     """
     def __init__(self, session_key=None):
         self.redis = redis.Redis(
             host=getattr(settings, "REDIS_HOST", 'localhost'),
             port=getattr(settings, "REDIS_PORT", 6379),
             db=getattr(settings, "REDIS_DB"))
-        super(SessionStore, self).__init__(session_key)
+        self._session_key = session_key
         self.modified = None
+        self.session_key = self._session_key
+        super(SessionStore, self).__init__(session_key)
 
 
     def load(self):
-        session_data = self.redis.get(self.session_key)
+        session_data = self.redis.get(self._session_key)
         if session_data is not None:
             return loads(session_data)
         self.create()
@@ -41,7 +43,7 @@ class SessionStore(SessionBase):
 
     def create(self):
         while True:
-            self.session_key = self._get_new_session_key()
+            self._session_key = self._get_new_session_key()
             try:
                 self.save(must_create=True)
             except CreateError:
@@ -54,11 +56,11 @@ class SessionStore(SessionBase):
         if must_create:
             # preserve=True -> SETNX
             result = self.redis.set(
-                self.session_key, dumps(self._get_session(no_load=must_create)))
+                self._session_key, dumps(self._get_session(no_load=must_create)))
             if result == 0: # 0 == not created, 1 == created.
                 raise CreateError
         else:
-            self.redis.set(self.session_key, dumps(self._get_session(no_load=must_create)),)
+            self.redis.set(self._session_key, dumps(self._get_session(no_load=must_create)),)
 
     def exists(self, session_key):
         if self.redis.exists(session_key):
